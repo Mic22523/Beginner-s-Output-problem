68618da0bad374640df4648fdb38f688aeeb5056
Mic22523 <160691865+Mic22523@users.noreply.github.com>
2024-03-20 11:57:26 +0800

    Create homework.py

---
+++ homework.py
@@ -0,0 +1,17 @@
+#Hw 1
+Minute = int(input("enter no. of minutes that you want to convert")
+Second = Minute*60
+print(Minute, "Minute =", Second, "Second")
+
+#Hw 2 
+print(" enter no. of birthday cards Cruella got for her last b'day")
+cruella_cards = int(input(">> ")) 
+jane_cards = cruella_cards*2 
+print("Jane recieves at least", jane_cards, "cards")
+
+#Hw 3 
+years = int(input("Enter your age/years:")) 
+days = years * 365 
+days_to_100_years = 36500 - days 
+print("you are", days, "days old. After", days_to_100_years, "days you will be 100")
+
